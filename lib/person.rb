cd11565ab3bccf2f8249a27c9c06319e4870fb8f
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-08 21:54:20 -0500

    commit tree 38bb67fcdc3ed0083b686aa6a297ad4cf9520a88

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,87 @@
 # your code goes here
+class Person
+    attr_reader :name, :hygiene, :happiness
+    attr_accessor :bank_account
+         def initialize(name) #, bank_account=25, happiness=8, hygiene=8)
+            @name = name
+            @bank_account = 25
+            @happiness = 8
+            @hygiene = 8
+        end
+        def happiness=(point)
+            @happiness = point
+
+            if @happiness > 10
+                @happiness = 10
+            elsif @happiness < 0
+                @happiness = 0
+            # else
+            #     @happiness
+            end
+        end
+        def hygiene=(point)
+           @hygiene = point
+            if @hygiene > 10
+                @hygiene = 10
+            elsif @hygiene < 0
+                @hygiene = 0
+            # else
+            #     @hygiene
+            end
+        end
+        def happy?
+            if happiness > 7
+                true
+            else 
+                false
+            end
+        end
+
+        def clean?
+            if hygiene > 7
+                true
+            else 
+                false
+            end
+        end
+        def get_paid(salary)
+            @bank_account += salary
+            "all about the benjamins"
+        end
+        def take_bath
+            self.hygiene += 4
+            "♪ Rub-a-dub just relaxing in the tub ♫"
+        end
+        def work_out
+            self.happiness += 2
+            self.hygiene -= 3
+            "♪ another one bites the dust ♫"
+        end
+        def call_friend(friend)
+            self.happiness += 3
+            friend.happiness += 3
+            "Hi #{friend.name}! It's #{self.name}. How are you?"
+        end
+        def start_conversation(friend, topic)
+            if topic == "politics"
+                friend.happiness -= 2
+                self.happiness -= 2
+               return "blah blah partisan blah lobbyist"
+            elsif topic == "weather"
+                friend.happiness += 1
+                self.happiness += 1
+               return "blah blah sun blah rain"
+            else
+                return "blah blah blah blah blah"
+            end
+        end
+    end
+
+    # stella = Person.new("Stella")
+    # p stella.work_out
+    # p stella.happiness
+
+    # puts "CALLING FELIX..."
+    # felix = Person.new("Felix")
+    # stella.happiness = 9
+    # p stella.happiness
